ffad763a064462094ea2020966aeb86a4509a817
Javier <jmartin@grupoloyal.com>
2019-03-28 12:26:51 +0100

    Excepciones

--- demo/src/main/java/com/ista/demo/controllers/ActoresController.java
+++ demo/src/main/java/com/ista/demo/controllers/ActoresController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
@@ -12,14 +13,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.ista.demo.exceptions.BadRequestException;
+import com.ista.demo.exceptions.NotFoundException;
 import com.ista.demo.model.Actor;
 import com.ista.demo.repositories.ActorRepository;
 
@@ -38,7 +43,7 @@ public class ActoresController {
 	public String view(@PathVariable Long id, Model model) {
 		Optional<Actor> item = dao.findById(id);
 		if(!item.isPresent())
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+			throw new NotFoundException();
 		model.addAttribute("elemento", item.get());
 		return "actores/view";
 	}
@@ -71,7 +76,7 @@ public class ActoresController {
 	public String editGET(@PathVariable Long id, Model model) {
 		Optional<Actor> item = dao.findById(id);
 		if(!item.isPresent())
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+			throw new NotFoundException();
 		model.addAttribute("modo", "edit");
 		model.addAttribute("action", "actores/" + id + "/edit");
 		model.addAttribute("elemento", item.get());
@@ -90,7 +95,7 @@ public class ActoresController {
 			mv.setViewName("actores/form");
 		} else {
 			if(!dao.findById(item.getActorId()).isPresent())
-				throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+				throw new NotFoundException();
 			dao.save(item);
 			mv.setViewName("redirect:/actores");
 		}
@@ -99,11 +104,16 @@ public class ActoresController {
 	
 	@GetMapping("/{id:\\d+}/delete")
 	public String editGET(@PathVariable Long id) {
-		try {
+//		try {
 			dao.deleteById(id);
-		} catch (Exception e) {
-		}
+//		} catch (Exception e) {
+//			throw new BadRequestException(e.getMessage(), e);
+//		}
 		return "redirect:/actores";
 	}
-	
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({ EmptyResultDataAccessException.class })
+    public void badRequest(Exception e) {
+    	throw new BadRequestException("nuevo: " + e.getMessage(), e);
+    }
 }
